134057c44bb3001c4ed876bf047d823297a4f4f0
Justin Chau <justin.jay.chau@gmail.com>
2019-10-27 06:39:40 -0400

    LiDAR Works!

--- hack-gt/gimbal/gimbal_vid_zmq.py
+++ hack-gt/gimbal/gimbal_vid_zmq.py
@@ -10,7 +10,7 @@ from rplidar import RPLidar
 
 print('Connecting to webcam...')
 
-stream = cv2.VideoCapture(2)
+stream = cv2.VideoCapture(4) #2
 
 print("Connecting to lidar...")
 lidar = RPLidar('/dev/ttyUSB0')
@@ -25,7 +25,7 @@ min_angle = 39
 max_angle = 360-min_angle
 
 # The IP address below is the IP address of the headset computer.
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.0.101:5556')
+sender = imagezmq.ImageSender(connect_to='tcp://localhost:5556') #tcp://192.168.0.101:5556
 
 time.sleep(2.0)
 jpeg_quality = 35 #0 to 100
@@ -35,28 +35,29 @@ resolution_height = int(0.75*resolution_width)
 field_of_view = 2 *min_angle
 degree_pixels = resolution_width/field_of_view
 
-distances = {}
+iterator = lidar.iter_scans(max_buf_meas=200)
 
 while True:
-    for i, measurement in enumerate(lidar.iter_measurments()):
-        angle = int(measurement[2]) + 39
+    scan = next(iterator)
+
+    (grabbed, frame) = stream.read()
+    frame = cv2.resize(frame, (resolution_width, resolution_height))
+    frame = cv2.flip( frame, -1 )
+
+    for measurement in scan:
+        angle = int(measurement[1]) + 39
         angle = angle % 360
-        distance = int(measurement[3])
-
-        (grabbed, frame) = stream.read()
-        frame = cv2.resize(frame, (resolution_width, resolution_height))
-
-        if(angle > 0 and angle < min_angle * 2):
-                distances[angle] = distance
-                print(angle)
-        for angle in distances:
-            cv2.circle(frame,(int(angle*degree_pixels),int(resolution_height/2)),3,(0,255,0),5)
+        distance = int(measurement[2])
         
-        cv2.imshow('frame', frame)
+        if(angle > 0 and angle < min_angle * 2):
+            if distance > 10 and distance < 500:
+                print(angle)
+                print (distance)
+                cv2.circle(frame,(int(angle*degree_pixels),int(resolution_height/2)),int(1000/distance),(0,255,0),5)
 
-        if not grabbed:
-            break
+    if not grabbed:
+        break
 
-        ret_code, jpg_buffer = cv2.imencode(
-            ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-        sender.send_jpg("Gimbal Camera",jpg_buffer)
+    ret_code, jpg_buffer = cv2.imencode(
+        ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
+    sender.send_jpg("Gimbal Camera",jpg_buffer)
