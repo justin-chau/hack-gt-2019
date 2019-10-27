59a14d21033b6d4192b7290c1a454d56bab671b0
Justin Chau <justin.jay.chau@gmail.com>
2019-10-27 02:38:20 -0400

    Fully switched to imagezmq.

--- hack-gt/gimbal/gimbal_vid_zmq.py
+++ hack-gt/gimbal/gimbal_vid_zmq.py
@@ -14,13 +14,13 @@ stream = cv2.VideoCapture(2)
 sender = imagezmq.ImageSender(connect_to='tcp://192.168.0.101:5556')
 
 time.sleep(2.0)  # allow camera sensor to warm up
-jpeg_quality = 95  # 0 to 100, higher is better quality, 95 is cv2 default
+jpeg_quality = 50  # 0 to 100, higher is better quality, 95 is cv2 default
 while True:  # send images as stream until Ctrl-C
     (grabbed, frame) = stream.read()
 
     if not grabbed:
         break
-    
+
     ret_code, jpg_buffer = cv2.imencode(
         ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
     sender.send_jpg("Gimbal Camera",jpg_buffer)
--- hack-gt/headset/headset_vid_zmp.py
+++ hack-gt/headset/headset_vid_zmp.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import imagezmq
 
-image_hub = imagezmq.ImageHub()
+image_hub = imagezmq.ImageHub(open_port='tcp://*:5556')
 
 print('Waiting for video stream...')
 
@@ -13,6 +13,6 @@ while True:  # show streamed images until Ctrl-C
     msg, jpg_buffer = image_hub.recv_jpg()
     image = cv2.imdecode(np.fromstring(jpg_buffer, dtype='uint8'), -1)
     # see opencv docs for info on -1 parameter
-    cv2.imshow(msg, image)  # 1 window for each RPi
+    cv2.imshow(msg, cv2.resize(image,(1920,1080)))  # 1 window for each RPi
     cv2.waitKey(1)
     image_hub.send_reply(b'OK')
